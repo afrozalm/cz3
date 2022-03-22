40eea5f14e61167333f87ef8b65e28287fbf71e6
sethzhao506@berkeley.edu <adamchang2000@berkeley.edu>
2022-03-22 16:16:25 -0700

    gcn-testing

--- example-app.cpp
+++ example-app.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <memory>
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui_c.h>
+#include <opencv2/features2d.hpp>
 #include <typeinfo>
 
 /**
@@ -23,11 +25,9 @@ int main(int argc, const char* argv[]) {
     	return -1;
   	}
 	// Deserialize the ScriptModule from a file using torch::jit::load().
-	std::string model_path = "C:/Users/OpenARK/Desktop/r2d2_test/traced_r2d2_WASF_N16.pt";
-	//model_path = "C:/Users/OpenARK/Desktop/openark_dependencies/okvis-master/traced_superpoint_model_cuda.pt";
-	std::unique_ptr<torch::jit::script::Module> model_;
+    std::shared_ptr<torch::jit::script::Module> model_;
 	try {
-		model_ = std::make_unique<torch::jit::script::Module>(torch::jit::load(argv[1]));
+		model_ = torch::jit::load(argv[1]);
 		model_->to(torch::kCUDA);
 	}
 	catch (const c10::Error& e) {
@@ -42,65 +42,50 @@ int main(int argc, const char* argv[]) {
 		std::cerr << "ERROR: Could not open camera" << std::endl;
 		return 1;
 	}
-	cv::namedWindow("CAM", CV_WINDOW_AUTOSIZE);
+	// cv::namedWindow("CAM", CV_WINDOW_AUTOSIZE);
 
 	// this will contain the image from the webcam
-	cv::Mat frame, grayFrame, kpt_frame;
-	int H = frame.rows, W = frame.cols;
-	torch::Tensor idxs = torch::arange(W*H).to(torch::kCUDA);
-	idxs = idxs.view({H, W});
-
+	cv::Mat frame, kpt_frame;
 
 	while (1) {
 		// capture the next frame from the webcam
-		std::cout << "looping\n";
 		std::vector<torch::jit::IValue> inputs;
-
 		camera >> frame;
-		cv::cvtColor(frame, grayFrame, CV_BGR2GRAY);
 
-		// show the image on the window
+        torch::Tensor img = torch::from_blob(frame.data, { 1, frame.rows, frame.cols, 1 }, torch::kByte).to(torch::kCUDA);
+        img = img.permute({ 0, 3, 1, 2 });
+		img = img.toType(torch::kFloat);
+		img = img.div(255);
+        inputs.push_back(img);
 
-		// pass gray image through r2d2
-		//torch::Tensor img_tensor = torch::from_blob(grayFrame.data, { 1, grayFrame.rows, grayFrame.cols, 1 }, torch::kByte).to(torch::kCUDA);
-		torch::Tensor img_tensor = torch::from_blob(frame.data, { 1, H, W, 3 }, torch::kByte).to(torch::kCUDA);
-		img_tensor = img_tensor.permute({ 0, 3, 1, 2 });
-		img_tensor = img_tensor.toType(torch::kFloat);
-		img_tensor = img_tensor.div(255);
-		inputs.push_back(img_tensor);
+        auto output = model_->forward(inputs).toTuple();
 
-		// execute the model
-		auto output = model_->forward(inputs);
+		// std::cout << "forward done\n";
+        auto pts  = output->elements()[0].toTensor().squeeze().to(torch::kCPU);
+        auto desc = output->elements()[1].toTensor().squeeze().to(torch::kCPU);
 
-		std::cout << "forward done\n";
-		//::cout << typeid(output).name() << "\n";
+        cv::Mat pts_mat(cv::Size(3, pts.size(0)), CV_32FC1, pts.data<float>());
+        // cv::Mat desc_mat(cv::Size(32, pts.size(0)), CV_8UC1, desc.data<unsigned char>());
 
-		torch::Tensor dsc_tensor = torch::squeeze(((torch::Tensor) output.toTensorList()[0]).to(torch::kCUDA)); // descriptors 128, W, H
-		torch::Tensor rel_tensor = torch::squeeze(((torch::Tensor) output.toTensorList()[1]).to(torch::kCUDA)); // reliability W, H
-		torch::Tensor rep_tensor = torch::squeeze(((torch::Tensor) output.toTensorList()[2]).to(torch::kCUDA)); // repeatibility W, H
+        std::vector<cv::KeyPoint> kpts;
+        // cv::Mat descriptors;
 
-		torch::Tensor score = torch::multiply(rel_tensor, rep_tensor);
-		torch::Tensor mask = score > 0.98;
-		std::cout << "score sizes" << score.sizes() << " mask : " << mask.sizes() << "\n";
-
-		torch::Tensor kpts_ = torch::masked_select(idxs, mask);
-		std::cout << "kpts_ size: " << kpts_.sizes() << "\n";
-
-		std::vector<cv::KeyPoint> kpts;
-		for (int i = 0; i < kpts_.sizes()[0]; i++) {
+        for (int i = 0; i < pts_mat.rows; i++) {
 			cv::KeyPoint kpt;
-			kpt.pt.x = kpts_[i].item<int>()%W;
-			kpt.pt.y = kpts_[i].item<int>()/W;
+			kpt.pt.x = pts_mat.at<float>(i, 0);
+			kpt.pt.y = pts_mat.at<float>(i, 1);
 			kpt.size = 10; // TODO - what should this be? affects geometry check
 			kpts.push_back(kpt);
 		}
 
-		cv::drawKeypoints(frame, kpts, kpt_frame, cv::Scalar(127, 200, 10), 4);
+        cv::imshow("Camera", frame);
+        cv::drawKeypoints(frame, kpts, kpt_frame, cv::Scalar(127, 200, 10), cv::DrawMatchesFlags::DEFAULT);
 
 		cv::imshow("Keypoints", kpt_frame);
 
+		// show the image on the window
 		// wait (10ms) for a key to be pressed
-		if (cv::waitKey(100) >= 0)
+		if (cv::waitKey(10) >= 0)
 			break;
 	}
 	return 0;
