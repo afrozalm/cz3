a7b3d954a0172d874374c45ccd7ea471a2b45f07
Afroz Alam <alam.afroz011@gmail.com>
2022-04-02 13:35:03 -0700

    working NMS

--- example-app.cpp
+++ example-app.cpp
@@ -19,6 +19,78 @@
  *
  */
 
+void NonMaximalSuppression( cv::Mat kpts_mat, \
+                            cv::Mat desc_mat, \
+                            std::vector<cv::KeyPoint>& _keypoints, \
+                            cv::Mat& descriptors, \
+                            int border, \
+                            int dist_threshold, \
+                            int img_width, \
+                            int img_height)
+{
+    cv::Mat kpt_grid = cv::Mat(cv::Size(img_width, img_height), CV_8UC1);
+    cv::Mat kpt_index = cv::Mat(cv::Size(img_width, img_height), CV_16UC1);
+
+    kpt_grid.setTo(0);
+    kpt_index.setTo(0);
+
+    for (int i = 0; i < kpts_mat.rows; i++){
+
+        int u = (int) kpts_mat.at<float>(i, 0);
+        int v = (int) kpts_mat.at<float>(i, 1);
+
+        // kpts_raw[i] = cv::Point2f(u, v);
+        kpt_grid.at<char>(v, u) = 1;
+        kpt_index.at<unsigned short>(v, u) = i;
+    }
+
+    cv::copyMakeBorder( kpt_grid, kpt_grid, dist_threshold, dist_threshold,
+                        dist_threshold, dist_threshold, cv::BORDER_CONSTANT, 0 );
+
+    for ( int i = 0; i < kpts_mat.rows; ++i )
+    {
+        int u = (int) kpts_mat.at<float>(i, 0) + dist_threshold;
+        int v = (int) kpts_mat.at<float>(i, 1) + dist_threshold;
+
+
+        if ( kpt_grid.at<char>(v, u) != 1 )
+            continue;
+
+        for ( int j = -dist_threshold; j <= dist_threshold; ++j )
+            for ( int k = -dist_threshold; k <= dist_threshold; ++k )
+            {
+                if ( j == 0 && k == 0)
+                    continue;
+
+                kpt_grid.at<char>(v + j, u + k) = 0;
+            }
+
+        kpt_grid.at<char>(v, u) = 2;
+    }
+
+    std::vector<int> valid_idxs;
+    for ( int u = dist_threshold; u < img_width - dist_threshold; ++u )
+        for ( int v = dist_threshold; v < img_height - dist_threshold; ++v )
+        {
+            if (kpt_grid.at<char>(v, u) == 2) {
+                int idx = (int) kpt_index.at<unsigned short>(v-dist_threshold, u-dist_threshold);
+                int x = (int) kpts_mat.at<float>(idx, 0);
+                int y = (int) kpts_mat.at<float>(idx, 1);
+                _keypoints.push_back( cv::KeyPoint( x, y, 1.0f ) );
+                valid_idxs.push_back(idx);
+            }
+        }
+
+    descriptors.create(valid_idxs.size(), 32, CV_8U);
+
+    for ( int i = 0; i < valid_idxs.size(); ++i )
+        for ( int j = 0; j < 32; ++j )
+        {
+            descriptors.at<unsigned char>(i, j) = desc_mat.at<unsigned char>(valid_idxs[i], j);
+        }
+}
+
+
 void nms(cv::Mat det, std::vector<cv::KeyPoint>& pts, int border, int dist_thresh,
          int img_width, int img_height){
 
@@ -112,17 +184,25 @@ int main(int argc, const char* argv[]) {
 	// cv::namedWindow("CAM", CV_WINDOW_AUTOSIZE);
 
 	// this will contain the image from the webcam
-	cv::Mat frame, img_frame, kpt_frame;
+    int img_width = 640;
+    int img_height = 480;
 
-    int img_width = 320;
-    int img_height = 240;
+	cv::Mat frame, img_frame, kpt_frame, tmp_frame;
+
+    int border = 16;
+    int dist_thresh = 8;
 
 	while (1) {
 		// capture the next frame from the webcam
 		std::vector<torch::jit::IValue> inputs;
 		camera >> frame;
 
-        cv::resize(frame, img_frame, cv::Size(img_width, img_height));
+        cv::resize(frame, tmp_frame, cv::Size(img_width/3, img_height/3));
+        cv::hconcat(tmp_frame, tmp_frame, img_frame);
+        cv::hconcat(img_frame, tmp_frame, img_frame);
+
+        cv::copyMakeBorder(img_frame, img_frame, 0, 2*img_height/3, 0, 0, cv::BORDER_CONSTANT, 0);
+
         torch::Tensor img = torch::from_blob(img_frame.data, { 1, img_frame.rows, img_frame.cols, 1 }, torch::kByte).to(torch::kCUDA);
         img = img.permute({ 0, 3, 1, 2 });
         img = img.toType(torch::kFloat32);
@@ -136,27 +216,19 @@ int main(int argc, const char* argv[]) {
         auto desc = output->elements()[1].toTensor().squeeze().to(torch::kCPU);
 
         cv::Mat pts_mat(cv::Size(3, pts.size(0)), CV_32FC1, pts.data<float>());
-        // cv::Mat desc_mat(cv::Size(32, pts.size(0)), CV_8UC1, desc.data<unsigned char>());
+        cv::Mat desc_mat(cv::Size(32, pts.size(0)), CV_8UC1, desc.data<unsigned char>());
 
         std::vector<cv::KeyPoint> kpts;
+        cv::Mat descriptors;
+
         // nms(pts_mat, kpts, border, dist_thresh, img_width, img_height);
+        NonMaximalSuppression(pts_mat, desc_mat, kpts, descriptors, border, dist_thresh, img_width, img_height);
 
-        // cv::Mat descriptors;
 
-        for (int i = 0; i < pts_mat.rows; i++) {
-		 	cv::KeyPoint kpt;
-		 	kpt.pt.x = pts_mat.at<float>(i, 0);
-		 	kpt.pt.y = pts_mat.at<float>(i, 1);
-		 	kpt.size = 1.0f; // TODO - what should this be? affects geometry check
-             if (kpt.pt.x > img_width || kpt.pt.y > img_height)
-                std::cout << "x: " << kpt.pt.x << ", y: " << kpt.pt.y << "\n";
-		 	kpts.push_back(kpt);
-		 }
-
-        //cv::imshow("Camera", frame);
         cv::drawKeypoints(frame, kpts, kpt_frame, cv::Scalar(127, 200, 10), cv::DrawMatchesFlags::DEFAULT);
 
 		cv::imshow("Keypoints", kpt_frame);
+        cv::imshow("Input Image", img_frame);
 
 		// show the image on the window
 		// wait (10ms) for a key to be pressed
